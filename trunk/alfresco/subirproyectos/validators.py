d921bfc5b62cd2c6bce47b2bef1f791855e79d29
Jesús Torres <jmtorres@ull.es>
2011-10-06 18:43:15 +0000

    Correciones del último merge y código de los validadores
    git-svn-id: https://svn.osl.ull.es/pdrcabrod@18 504b1ee8-bf37-48d5-949c-1573cdd6fe78

---
+++ trunk/alfresco/subirproyectos/validators.py
@@ -0,0 +1,13 @@
+from django.core.validators import RegexValidator
+
+import mimetypes
+
+from subirproyectos import settings
+
+NIUValidator = RegexValidator(regex ='alu\d{10}', message='Ej: alu0100353303')
+
+def file_format(value):
+    format = mimetypes.guess_type(request.FILES['file'].name)
+    formats = [mimetype for mimetype, name in SELECCION_FORMATO]
+    if format not in formats:
+        raise ValidationError('Formato de fichero no válido' % value)
