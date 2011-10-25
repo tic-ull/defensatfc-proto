5c67d96f5b9eb9dbc90a0b6630e4ac58a4fb47f0
Pedro Cabrera <pdrcabrod@gmail.com>
2011-10-25 16:23:30 +0000

    git-svn-id: https://svn.osl.ull.es/pdrcabrod@26 504b1ee8-bf37-48d5-949c-1573cdd6fe78

--- trunk/alfresco/subirproyectos/management/commands/syncalfresco.py
+++ trunk/alfresco/subirproyectos/management/commands/syncalfresco.py
@@ -9,7 +9,6 @@ class Command(NoArgsCommand):
 
     def handle(self, *args, **options):
         centros = Centro.objects.filter(alfresco_uuid = "")
-        #centros = Centro.objects.all() 
         #TODO si no hay centros, no hacer login en alfresco, añadir type a settings
 	cml = Alfresco().cml()
 	for centro in centros:
@@ -21,18 +20,6 @@ class Command(NoArgsCommand):
 	    cml.create (TFC_UUID, "{http://www.ull.es/2011/10/04/pfc.xsd}centro", properties_centro)
 	    e = Centro.objects.get(nombre = centro.nombre)
 	    titulaciones = e.titulacion_set.filter(alfresco_uuid = "")
-	    #for titulacion in titulaciones:
-	      #properties_titulacion = {
-	    #'cm:name': titulacion.nombre,
-	    #'{http://www.alfresco.org/model/content/1.0}name' : titulacion.nombre,
-	    #'pfc:codigoPlan' : titulacion.codigo_plan,
-	    #'pfc:anyoComienzoPlan' : titulacion.anyo_comienzo_plan,
-	    #'pfc:titulacionVigente' : titulacion_vigente	
-	     #}
-	     #cml.create (TFC_UUID, "{http://www.ull.es/2011/10/04/pfc.xsd}titulacion", properties_titulacion)
-	    
-	    #centro.alfresco_uuid = results[0].destination.uuid
-	    #centro.save()
 	results = cml.do()
 	cml = Alfresco().cml()
 	ncentros = 0
