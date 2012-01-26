cccb9607c38d140620b7fda162c58f110a941cba
Jesús Torres <jmtorres@ull.es>
2012-01-26 03:15:55 +0000

    corregido el error al mover el put
    git-svn-id: https://svn.osl.ull.es/pdrcabrod@76 504b1ee8-bf37-48d5-949c-1573cdd6fe78

--- trunk/tfc_webapps/__init__.py
+++ trunk/tfc_webapps/__init__.py
@@ -2,5 +2,6 @@ from os.path import dirname, join, abspath
 from sys import path
 
 #path.append(dirname(__file__))
+path.append(join(dirname(abspath(__file__)), "packages"))
 path.append(join(dirname(abspath(__file__)), "packages/suds-timestamp"))
 path.append(join(dirname(abspath(__file__)), "packages/py-wkhtmltox"))
